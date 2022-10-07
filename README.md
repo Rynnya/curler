50bacdf7a5050447516b52510e3ce8128dc6790a
Rynnya <ancowi@protonmail.com>
2022-10-08 02:44:27 +0300

    initial commit

---
+++ README.md
@@ -0,0 +1,15 @@
+# curlEr
+
+curlEr is a simple async http client, created on top of curl (https://curl.se/).
+
+## Features
+
+* Simple to install - 2 files, 1 header and 1 source
+* Simple to use - create a factory, create a builder, fill up the builder and push the request
+* Supports C++14 and higher
+* Efficient because utilizes a curl_multi_* API
+* Uses only STL and curl library
+
+## License
+
+Licensed under MIT license (https://opensource.org/licenses/MIT)
---
+++ main.cpp
@@ -0,0 +1,26 @@
+﻿#include "curl.hh"
+
+int main() {
+
+    curl::Factory factory { /* maxAmountOfConcurrentConnections = 0 (unlimited), maxConnectionTimeoutInMilliseconds = 0 (300 seconds default) */ };
+
+    curl::Builder blueprint = factory.createRequest("https://www.example.com/");
+    blueprint
+        .setOnExceptionCallback([](curl::ExceptionType exType, std::exception_ptr exPtr) {
+            /* do something with exType and exPtr, this will be called when any of your callbacks caused an exception */
+        })
+        .setOnErrorCallback([](curl::Response& resp) {
+            /* do something with resp */
+        })
+        .setPreRequestCallback([](const curl::Builder& builder, const std::string& url) {
+            /* do something with builder which is 'blueprint' and url which is complete url with host, path and query */
+        })
+        .setPostRequestCallback([](curl::Response& resp) {
+            /* do something with resp */
+        });
+
+    factory.pushRequest(blueprint);
+
+    return 0;
+}
+
